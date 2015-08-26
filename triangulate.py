3719a9ef94c8c72874b716517d821ee56f8f74d2
Mark Fickett <mark.fickett@gmail.com>
2015-08-25 20:17:44 -0400

    Implement position calculation.

--- triangulate.py
+++ triangulate.py
@@ -7,6 +7,7 @@
 # surveyed line segments and that target. For example, output like BC 51.22
 # would mean that 51.22 units along line segment BC intercects the target.
 
+import math
 import sys
 
 
@@ -14,7 +15,8 @@ class MeasuredPoint(object):
   def __init__(self, name):
     self.name = name
     self.neighbors_and_distances = []
-    self.pos = None
+    self.x = None
+    self.y = None
 
   def AddEdge(self, neighbor, distance):
     if self.name == neighbor.name:
@@ -23,19 +25,29 @@ class MeasuredPoint(object):
 
   def ComputePosition(self):
     for neighbor, _ in self.neighbors_and_distances:
-      if neighbor.pos is None:
+      if neighbor.x is None or neighbor.y is None:
         raise RuntimeError(
             'Cannot calculate position of %r, neighbor %r has no position.'
             % (self.name, neighbor.name))
     n = len(self.neighbors_and_distances)
     if n == 0:
-      self.pos = (0.0, 0.0)
+      self.x, self.y = (0.0, 0.0)
       return
     if n == 1:
-      origin, dx = self.neighbors_and_distances[0]
-      self.pos = (origin.pos[0] + dx, origin.pos[1])
+      origin, d = self.neighbors_and_distances[0]
+      theta = math.pi * 3.0 / 4.0  # arbitrary direction
+      self.x = origin.x + d * math.cos(theta)
+      self.y = origin.y + d * math.sin(theta)
       return
-    raise NotImplementedError()
+    if n != 2:
+      raise RuntimeError('Two neighbors required for nontrivial cases.')
+
+    x0, y0 = GetThirdTrianglePoint(*self.neighbors_and_distances)
+    x1, y1 = GetThirdTrianglePoint(*self.neighbors_and_distances, opposite=True)
+    if y0 > y1:
+      self.x, self.y = x0, y0
+    else:
+      self.x, self.y = x1, y1
 
   def GetIntersections(self, segment_start, segment_end):
     """Generates a list of (neighbor_point, distance) tuples.
@@ -46,6 +58,38 @@ class MeasuredPoint(object):
     that distance is in [0, length of segment to neighbor] it yields the result.
     """
     raise NotImplementedError()
+
+
+def GetThirdTrianglePoint((point_a, b), (point_b, a), opposite=False):
+  # Givens: A triangle ABC where we are computing the point C, and the edge
+  # lengths are named for their opposing points.
+  ax, ay = point_a.x, point_a.y
+  bx, by = point_b.x, point_b.y
+  c = math.sqrt((ax - bx)**2 + (ay - by)**2)
+  print 'ax, ay =', ax, ',', ay
+  print 'b =', b
+  print 'bx, by =', bx, ',', by
+  print 'a =', a
+  print 'c =', c
+
+  # Calculate the angle CAB using law of consines.
+  alpha = math.acos((b**2 + c**2 - a**2) / (2 * b * c))
+  print 'alpha =', alpha
+
+  # Calculate the angle between AB and the x axis.
+  sub_alpha = math.acos((bx - ax) / c)
+  if by < ay:  # third and fourth quadrant
+    sub_alpha = 2 * math.pi - sub_alpha
+  print 'sub_alpha =', sub_alpha
+
+  # The sum alpha + sub_alpha is the angle from the x axis to AC. Use the
+  # definition of cosine to find C.
+  full_alpha = sub_alpha - alpha if opposite else sub_alpha + alpha
+  cx = ax + b * math.cos(full_alpha)
+  cy = ay + b * math.sin(full_alpha)
+  print 'cx, cy =', cx, ',', cy
+
+  return cx, cy
 
 
 def ParseLine(raw_line):
@@ -90,6 +134,7 @@ if __name__ == '__main__':
         neighbor = points[neighbor_name]
         point.AddEdge(neighbor, distance)
       point.ComputePosition()
+      print '%r\t%f\t%f' % (point.name, point.x, point.y)
   sys.exit(0)
   for point in points.itervalues():
     if point in (first_point, last_point):
